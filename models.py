a81adacf27a4520479f6ac8322a53d304a8a5a27
Nijita <nik.dyabdenko@mail.ru>
2024-11-27 02:00:01 +0300

    Добавил хэширование (с помощью библиоетки werkzeug.security) -- в класс User из файла models.py добавил функции set_password и check_password

--- models.py
+++ models.py
@@ -1,15 +1,29 @@
 from datetime import datetime, timezone
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash # Пока используем библиотеку для хэширования, позже, позможно, напишем свой модуль для хэширования паролей и других данных, если потребуется
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic') # На более высоком уровне (то есть в самой БД этого видно не будет) также создается поле author для объекта класса Post (можно, например, вызвать post.author для некоторой post)
 
+    # При вызове User.query.get('user.id') --> <User user.username>
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    # Устанавливаем пароль пользователю
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+      
+    # Проверка пароля    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+ 
+ 
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +32,7 @@ class Post(db.Model):
     file_body = db.Column(db.String(140)) # Потом заменим на file_path = db.Column(db.String(255), nullable=False, unique=True)
     timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), index=True) 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user — это имя таблицы базы данных, которую Flask-SQLAlchemy автоматически устанавливает как имя класса модели, преобразованного в нижний регистр
+    
     
     def __repr__(self):
         return '<Post {}>'.format(self.title)
