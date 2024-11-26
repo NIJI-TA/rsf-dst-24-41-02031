94d356334cfe07df8e7c5f3dbe4a6eed99c3acdc
Nijita <nik.dyabdenko@mail.ru>
2024-11-26 20:10:34 +0300

    Удалил FDataBase.py и sq_db.sql -- они больше не нужны

--- sq_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-id integer PRIMARY KEY AUTOINCREMENT,
-name text NOT NULL,
-email text NOT NULL,
-password text NOT NULL,
-role TEXT DEFAULT 'user',
-time integer NOT NULL
-);
