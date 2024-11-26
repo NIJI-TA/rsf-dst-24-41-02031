c8526402decf6e52ef4ab775bd9e3a44c998fc1e
Nijita <nik.dyabdenko@mail.ru>
2024-11-26 19:56:48 +0300

    Начал работу с Flask-SQLAlchemy и миграцией баз данных -- создал в mjdels поля User (для обычных пользователей и участников проекта, которые смогут загружать контент на сайт -- что еще необходимо сделать) и Posts (статьи, которые будут загружаться на сайт и которые можно будет скачать) для БД SQLite

--- app.py
+++ app.py
@@ -1,60 +1,64 @@
-from flask import Flask, g, url_for, render_template, flash, request, redirect
-import sqlite3
-import os
-from werkzeug.security import generate_password_hash, check_password_hash
-from FDataBase import FDataBase 
-from forms import LoginForm
+from flask import Flask, url_for, render_template, flash, request, redirect
+from forms import LoginForm # Берем наши формы как поля классов в файле forms
+from config import Config # Берем нашу конфигурацию как класс из файла config.py 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
-# Конфигурация
-DATABASE = '/tmp/app.db'
-DEBUG = True
-SECRET_KEY = 'sdlfhh5jhhj241jh25g1hk41gjk4'
+# Дополнительные библиотеки
+from werkzeug.security import generate_password_hash, check_password_hash # Генерация hash-а для паролей
+# from FDataBase import FDataBase # Класс для работы с БД
 
+
+# Создание объекта-приложения как экземпляра класса Flask из пакета flask, и настройка конфигурации
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object(Config)
 
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'app.db')))
-
-
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
-    return conn
+# Базы данных и миграции
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-def create_db():
-    """Вспомогательная функция для создания таблиц БД"""
-    db = connect_db()
-    with app.open_resource('sq_db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
+import models # Импортируем модели для наших БД
+
+# def connect_db():
+#     conn = sqlite3.connect(app.config['DATABASE'])
+#     conn.row_factory = sqlite3.Row
+#     return conn
 
 
-def get_db():
-    """Соединение с БД, если оно еще не установлено"""
-    if not hasattr(g, 'link_db'):
-        g.link_db = connect_db()
-    return g.link_db
+# def create_db():
+#     """Вспомогательная функция для создания таблиц БД"""
+#     db = connect_db()
+#     with app.open_resource('sq_db.sql', mode='r') as f:
+#         db.cursor().executescript(f.read())
+#     db.commit()
+#     db.close()
 
 
-@app.teardown_appcontext
-def close_db(error):
-    """Закрываем соединение с БДб если оно было установлено"""
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
+# def get_db():
+#     """Соединение с БД, если оно еще не установлено"""
+#     if not hasattr(g, 'link_db'):
+#         g.link_db = connect_db()
+#     return g.link_db
 
 
-dbase = None
+# @app.teardown_appcontext
+# def close_db(error):
+#     """Закрываем соединение с БДб если оно было установлено"""
+#     if hasattr(g, 'link_db'):
+#         g.link_db.close()
 
 
-@app.before_request
-def before_request():
-    """Установление соединения с БД перед выполнением запроса"""
-    global dbase
-    db = get_db()
-    dbase = FDataBase(db)
+# dbase = None
+
+
+# @app.before_request
+# def before_request():
+#     """Установление соединения с БД перед выполнением запроса"""
+#     global dbase
+#     db = get_db()
+#     dbase = FDataBase(db)
 
 
 @app.route("/")
---
+++ models.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timezone
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False, index=True)
+    description = db.Column(db.Text, nullable=True)
+    file_body = db.Column(db.String(140)) # Потом заменим на file_path = db.Column(db.String(255), nullable=False, unique=True)
+    timestamp = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), index=True) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user — это имя таблицы базы данных, которую Flask-SQLAlchemy автоматически устанавливает как имя класса модели, преобразованного в нижний регистр
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.title)
+    
+    
