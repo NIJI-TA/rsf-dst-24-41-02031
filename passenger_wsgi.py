501dda94d9a85673046b53d6d7273026d084e9c4
Nijita <nik.dyabdenko@mail.ru>
2024-12-06 22:16:48 +0300

    Исправил ошибку в passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-INTERP = os.path.expanduser("/var/www/u0000005/data/flaskenv/bin/python")
+INTERP = os.path.expanduser("/var/www/u2931097/data/flaskenv/bin/python")
 if sys.executable != INTERP:
    os.execl(INTERP, INTERP, *sys.argv)
 
