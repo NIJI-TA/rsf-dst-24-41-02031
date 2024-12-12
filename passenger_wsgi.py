df15b5a870039f890381ff0c67b7234e1d3cff6c
Nijita <nik.dyabdenko@mail.ru>
2024-12-12 12:52:12 +0300

    Дизайн, адаптивность, исправление ошибок, мета-теги

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,12 @@
 import sys
 import os
 
+os.environ["OMP_NUM_THREADS"] = "4"
+os.environ["OPENBLAS_NUM_THREADS"] = "4"
+os.environ["MKL_NUM_THREADS"] = "6"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "4" 
+os.environ["NUMEXPR_NUM_THREADS"] = "6"
+
 INTERP = os.path.expanduser("/var/www/u2931097/data/flaskenv/bin/python")
 if sys.executable != INTERP:
    os.execl(INTERP, INTERP, *sys.argv)
